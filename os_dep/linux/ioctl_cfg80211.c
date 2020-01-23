08838ef008df7e62dbf4aec55924270dce12a6f6
110crosswalk <avg.holdingsllc@gmail.com>
2020-01-23 07:04:12 -0500

    Update ioctl_cfg80211.c

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -577,9 +577,8 @@ static u64 rtw_get_systime_us(void)
 
 	return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39) && LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0))
-	struct timespec64 ts;
-    ktime_get_boottime_ts64(&ts);
-
+	struct timespec ts;
+	getboottime(&ts);
 	return ((u64)ts.tv_sec * 1000000) + ts.tv_nsec / 1000;
 #else
 	struct timeval tv;
